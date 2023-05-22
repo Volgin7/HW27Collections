95c494ac4009c09d46205ed564e3b19036df3360
Volgin7 <8612020@gmail.com>
2023-05-22 23:13:39 +0300

    initial commit

---
+++ src/main/java/pro/sky/hw27collections/Hw27CollectionsApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.hw27collections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw27CollectionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw27CollectionsApplication.class, args);
+    }
+
+}
